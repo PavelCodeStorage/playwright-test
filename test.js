ca11ca42febb0903f6048c00fb912f69bd2f5775
PavelCodeStorage <paul.mail.dovgan@gmail.com>
2021-05-23 15:35:27 +0300

    changed test

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 describe('Simple Math Test', () => {
- it('should return 2', () => {
+ it('should return 3', () => {
         assert.equal(2 + 1, 3);
     });
 });
