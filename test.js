20dc719f3c0c74bc1a9a6d4da0b6d7c23c20be8a
PavelCodeStorage <paul.mail.dovgan@gmail.com>
2021-05-26 10:23:56 +0300

    added few changes

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const assert = require('assert');
 describe('Simple Math Test', () => {
- it('should return 6', () => {
-        assert.equal(3 + 3, 6);
+ it('should return 8', () => {
+        assert.equal(4 + 4, 8);
     });
 });
