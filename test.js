702dc6d18861dfe4b2e0801b5d8c3a7b0241bdab
Pavel Dovgan <paveldovgan@Pavels-MacBook-Air.local>
2026-03-11 20:47:53 -0700

    added pipe to github

--- test.js
+++ test.js
@@ -1,6 +1,11 @@
 const assert = require('assert');
+
 describe('Simple Math Test', () => {
- it('should return 3', () => {
-        assert.equal(2 + 1, 3);
-    });
+  it('@tag1', () => {
+    assert.equal(2 + 1, 3);
+  });
+
+  it('@tag2', () => {
+    assert.equal(2 + 3, 5);
+  });
 });
